545ef4b1bf40aa70e6c257d4b06920f80e541e95
bonitoflakes101 <xgrendonila2020@plm.edu.ph>
2025-03-28 03:05:37 +0800

    Register models - forgot to add lol

--- inventory_backend/pcounts/admin.py
+++ inventory_backend/pcounts/admin.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+admin.site.register(Products)
+admin.site.register(ItemMasterData)
+admin.site.register(Employee)
+admin.site.register(CyclicCount)
