5d868630555c5c25c27bdd9e9095060d81571282
Tristan <tgjvocampo2021@plm.edu.ph>
2025-03-28 15:31:57 +0800

    Database fix

--- inventory_backend/inventory_backend/settings.py
+++ inventory_backend/inventory_backend/settings.py
@@ -96,8 +96,12 @@ WSGI_APPLICATION = 'inventory_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'erp_db',
+        'USER': 'erp_user',
+        'PASSWORD': 'tris123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
