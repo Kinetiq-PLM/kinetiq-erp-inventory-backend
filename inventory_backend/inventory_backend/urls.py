7995608707acad2534938d150f3bf89cfdd314f1
Tristan <tgjvocampo2021@plm.edu.ph>
2025-03-30 01:53:26 +0800

    API Path fixed

--- inventory_backend/inventory_backend/urls.py
+++ inventory_backend/inventory_backend/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('feature_depreciation_report.urls')),
 ]
+
+#  path('api/', include('feature_stockflow.urls')),
