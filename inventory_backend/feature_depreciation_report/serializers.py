34439e07a634bdc5635c0d12e8c0b4fec8430100
Tristan <tgjvocampo2021@plm.edu.ph>
2025-04-17 01:35:55 +0800

    Updated Shelf Life

--- inventory_backend/feature_depreciation_report/serializers.py
+++ inventory_backend/feature_depreciation_report/serializers.py
@@ -1,34 +1,16 @@
         
 from rest_framework import serializers
-from .models import ExpiryReport
+from .models import ExpiryReportData
 
-class AssetsDeprecationReportSerializer(serializers.Serializer):
-    deprecation_report_id = serializers.CharField()
-    asset_id = serializers.CharField(source="inventory_item_id__asset_id")
-    asset_name = serializers.CharField(source="inventory_item_id__asset_id__asset_name")
-    status = serializers.CharField()
-    reported_date = serializers.DateTimeField()
-    quantity = serializers.IntegerField(source="inventory_item_id__current_quantity")
-    
-
-class RawMatDeprecationReportSerializer(serializers.Serializer):
-    deprecation_report_id = serializers.CharField()
-    material_id = serializers.CharField(source="inventory_item_id__material_id")
-    material_name = serializers.CharField(source="inventory_item_id__material_id__material_name")
-    status = serializers.CharField()
-    reported_date = serializers.DateTimeField()
-    quantity = serializers.IntegerField(source="inventory_item_id__current_quantity")
-
-class ProductDeprecationReportSerializer(serializers.Serializer):
-    deprecation_report_id = serializers.CharField()
-    status = serializers.CharField()
-    reported_date = serializers.DateTimeField()
-    productdocu_id = serializers.CharField(source="inventory_item_id__productdocu_id")
-    product_name = serializers.CharField(source="inventory_item_id__productdocu_id__product_id__product_name")
-    expiry = serializers.DateField(source="inventory_item_id__expiry")
-    quantity = serializers.IntegerField(source="inventory_item_id__current_quantity")
-
-class ExpiryReportSerializer(serializers.ModelSerializer):
+class ExpiryReportDataSerializer(serializers.ModelSerializer):
     class Meta:
-        model = ExpiryReport
-        fields = ['expiry_report_id', 'expiry_report_status', 'item_management', 'item_identification', 'current_quantity', 'expiry', 'warehouse_id']
+        model = ExpiryReportData
+        fields = ['expiry_report_id',
+                  'item_name',
+                  'item_type',
+                  'item_management', 'item_management_id',
+                  'expiry',
+                  'current_quantity',
+                  'expiry_report_status',]
+
+        
--- inventory_backend/feature_depreciation_report/views.py
+++ inventory_backend/feature_depreciation_report/views.py
@@ -1,70 +1,8 @@
 from rest_framework import generics
-# from .models import DeprecationReport
-from .serializers import (
-    ExpiryReportSerializer,
-)
+from .models import ExpiryReportData
+from .serializers import ExpiryReportDataSerializer
 
-# class ProductDeprecationReportList(generics.ListCreateAPIView):
-#     serializer_class = ProductDeprecationReportSerializer
-
-#     def get_queryset(self):
-#         return DeprecationReport.objects.filter(
-#             inventory_item_id__productdocu_id__isnull=False  
-#         ).select_related(
-#             "inventory_item_id",
-#             "inventory_item_id__productdocu_id__product_id"
-#         ).values(
-#             "deprecation_report_id",
-#             "status",
-#             "reported_date",
-#             "inventory_item_id",
-#             "inventory_item_id__current_quantity",
-#             "inventory_item_id__productdocu_id",  
-#             "inventory_item_id__expiry",
-#             "inventory_item_id__productdocu_id__product_id__product_name",
-#         )
-
-
-# class AssetsDeprecationReportList(generics.ListCreateAPIView):
-#     serializer_class = AssetsDeprecationReportSerializer
-
-#     def get_queryset(self):
-#         return DeprecationReport.objects.filter(
-#             inventory_item_id__asset_id__isnull=False  
-#         ).select_related(
-#             "inventory_item_id",
-#             "inventory_item_id__asset_id",
-#         ).values(
-#             "deprecation_report_id",
-#             "status",
-#             "reported_date",
-#             "inventory_item_id",          
-#             "inventory_item_id__current_quantity",
-#             "inventory_item_id__asset_id",  
-#             "inventory_item_id__asset_id__asset_name",
-#         )
-
-
-# class RawMatDeprecationReportList(generics.ListCreateAPIView):
-#     serializer_class = RawMatDeprecationReportSerializer
-
-#     def get_queryset(self):
-#         return DeprecationReport.objects.filter(
-#             inventory_item_id__material_id__isnull=False  # Only include records where material_id is NOT NULL
-#         ).select_related(
-#             "inventory_item_id",
-#             "inventory_item_id__material_id",
-           
-#         ).values(
-#             "deprecation_report_id",
-#             "status",
-#             "reported_date",
-#             "inventory_item_id",
-#             "inventory_item_id__current_quantity",
-#             "inventory_item_id__material_id",
-#             "inventory_item_id__material_id__material_name", 
-#         )
-
-class ExpiryReportList(generics.ListCreateAPIView):
-    serializer_class = ExpiryReportSerializer
+class ExpiryReportDataView(generics.ListCreateAPIView):
+    queryset = ExpiryReportData.objects.all()
+    serializer_class = ExpiryReportDataSerializer
     
